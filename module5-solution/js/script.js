b2b1211a28750e595d97179b42e3a00567d45089
Oxana <105598536+Oxsana1992@users.noreply.github.com>
2022-09-25 18:53:07 +0300

    Add files via upload

---
+++ module5-solution/js/script.js
@@ -0,0 +1,22 @@
+// Event handling
+document.addEventListener("DOMContentLoaded", 
+	function (event) {
+		// Unobstrusive event binding
+		document.querySelector("button")
+			.addEventListener("click", function () {
+					var self = this;
+					var name = "";
+
+					// Call server to get the name
+					$ajaxUtils
+					.sendGetRequest("/data/name.txt", 
+						function (request) {
+							self.name = request.responseText;
+							console.log(self.name);
+						});
+
+				document.querySelector("#content")
+					.innerHTML = "<h2>Hello " + self.name + "!";
+			});
+	}
+);

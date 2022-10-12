0c131a208528481e83d8f4813a750ea98e300d89
Oxsana1992 <o-loktionowa@yandex.ru>
2022-10-12 20:49:11 +0300

    12 10 2022 20 49

---
+++ JS-example/js/script.js
@@ -0,0 +1,11 @@
+// 1
+const num = 42
+// let result
+// if (num > 20) {
+// 	result = 'More then 20'
+// } else {
+// 	result = 'Less then 20'
+// }
+// через тернарные операторы
+const result = num > 20 ? 'More then 20' : 'Less then 20'
+console.log(result)

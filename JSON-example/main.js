038831be04cc523c91a6dfb364217ad8695aa1d0
Oxsana1992 <o-loktionowa@yandex.ru>
2022-10-16 23:10:22 +0300

    16 10 2022 23 10

--- JSON-example/main.js
+++ JSON-example/main.js
@@ -1,42 +1,46 @@
-let fs = require('fs');
-let express = require('express');
-let bodyParser = require('body-parser');
+let fs = require("fs");
+let express = require("express");
+let bodyParser = require("body-parser");
 
 let app = express();
-let urlencodeParser = bodyParser.urlencoded({extended: false});
+let urlencodeParser = bodyParser.urlencoded({ extended: false });
 
-app.set('view engine', 'ejs');
-app.use('/public', express.static('public'));
+app.set("view engine", "ejs");
+app.use("/public", express.static("public"));
 
 // Чтение из файла
-let file = fs.readFileSync('objects.json', 'utf8');
-let jsObjectData = JSON.parse(file)
+let file = fs.readFileSync("objects.json", "utf8");
+let jsObjectData = JSON.parse(file);
 // Форма
-app.post('/add', urlencodeParser, (reg, res) => {
-    if(!reg.body) return res.sendStatus(400);
-    console.log(reg.body);
-    let jsonData = fs.readFileSync('objects.json', 'utf8');
-    let all = jsonData.substring(0, jsonData.length-1)+', '+JSON.stringify(reg.body)+']';
-    console.log(all);
-    fs.writeFileSync('objects.json', all, error => {
-        if(error) throw error;
-        console.log('Асинхронная запись файла завершена.') 
-    });
-    res.render('add');
+app.post("/add", urlencodeParser, (req, res) => {
+  if (!req.body) return res.sendStatus(400);
+  console.log(req.body);
+  let jsonData = fs.readFileSync("objects.json", "utf8");
+  let all =
+    jsonData.substring(0, jsonData.length - 1) +
+    ", " +
+    JSON.stringify(req.body) +
+    "]";
+  console.log(all);
+  fs.writeFileSync("objects.json", all, (error) => {
+    if (error) throw error;
+    console.log("Асинхронная запись файла завершена.");
+  });
+  res.render("add");
 });
-// Сервер 
-app.listen(3000)
+// Сервер
+app.listen(3000);
 
-app.get('/', (reg, res) => {
-    res.render('add');
-})
+app.get("/", (req, res) => {
+  res.render("add");
+});
 
-app.get('/:name', (reg, res) => {
-    if (reg.params.name === 'add') {
-        res.render('add');
-    } else if(reg.params.name === 'view'){
-        res.render('vew', {data: jsObjectData});
-    } else {
-        res.sendFile(__dirname + '/404.html')
-    }
+app.get("/:name", (req, res) => {
+  if (req.params.name === "add") {
+    res.render("add");
+  } else if (req.params.name === "view") {
+    res.render("view", { data: jsObjectData });
+  } else {
+    res.sendFile(__dirname + "/404.html");
+  }
 });
